5714a4003e178d8212995346a6cdf6a06b02d65e
Slumhunden <kroghbastin@gmail.com>
2023-04-26 10:36:35 +0200

    1.eksamensøvelse

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+window.addEventListener("load", initApp);
+
+let users = [];
+async function initApp() {
+	users = await getUsers();
+	console.log(users);
+}
+async function getUsers() {
+	const response = await fetch("users.json");
+	const data = await response.json();
+	return data;
+}
+function showUsers(user){
+    const html = /*html*/ `
+    <li>Name: ${user.name} Active:${user.active} </li>`;
+    document.querySelector("#userlist").insertAdjacentHTML("beforeend",html)
+}
