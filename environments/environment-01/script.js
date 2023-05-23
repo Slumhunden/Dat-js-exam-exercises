233c9515ec66dc5bee5dbc1029df5bed02eda3a6
Slumhunden <kroghbastin@gmail.com>
2023-05-23 09:09:57 +0200

    exercise 2

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,19 +1,63 @@
-"use strict";
+//exercise 00 //
 
-window.addEventListener("load", initApp);
+// "use strict";
 
-let users = [];
-async function initApp() {
-	users = await getUsers();
-	console.log(users);
+// window.addEventListener("load", initApp);
+
+// let users = [];
+// async function initApp() {
+// 	users = await getUsers();
+// 	console.log(users);
+// }
+// async function getUsers() {
+// 	const response = await fetch("users.json");
+// 	const data = await response.json();
+// 	return data;
+// }
+// function showUsers(user){
+//     const html = /*html*/ `
+//     <li>Name: ${user.name} Active:${user.active} </li>`;
+//     document.querySelector("#userlist").insertAdjacentHTML("beforeend",html)
+// }
+
+//exercise 02 //
+
+window.addEventListener("load",initApp);
+
+let users = []
+async function initApp(){
+	users = await fetchUsers()
+	console.log(users)
+	for (
+		const user of users){
+			showList(user)
+		}
+countRoles();
 }
-async function getUsers() {
+async function fetchUsers(){
 	const response = await fetch("users.json");
 	const data = await response.json();
-	return data;
+	return data
 }
-function showUsers(user){
-    const html = /*html*/ `
-    <li>Name: ${user.name} Active:${user.active} </li>`;
-    document.querySelector("#userlist").insertAdjacentHTML("beforeend",html)
+
+function showList(user){
+	const html = /*html*/ `
+	<li>Users: ${user.name}</li>`
+	document.querySelector("#userlist").insertAdjacentHTML("beforeend",html)
+}
+function countRoles(){
+let admin = 0
+let guest = 0
+let bruger = 0 
+for (const user of users){
+if (user.role === "admin"){
+admin++
+}else if (user.role === "user"){
+bruger++
+}else if (user.role === "guest"){
+	guest++
+}}
+document.querySelector("#admin-count").textContent = admin;
+document.querySelector("#guest-count").textContent = guest;
+document.querySelector("#user-count").textContent = bruger;
 }
