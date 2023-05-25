f3ecec6bb2f2357e9f4926932652a66d5ceecf5d
Slumhunden <kroghbastin@gmail.com>
2023-05-25 11:20:05 +0200

    eksamensøvelser

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -22,42 +22,78 @@
 
 //exercise 02 //
 
-window.addEventListener("load",initApp);
+// window.addEventListener("load",initApp);
 
-let users = []
-async function initApp(){
-	users = await fetchUsers()
-	console.log(users)
-	for (
-		const user of users){
-			showList(user)
-		}
-countRoles();
-}
-async function fetchUsers(){
-	const response = await fetch("users.json");
-	const data = await response.json();
-	return data
-}
+// let users = []
+// async function initApp(){
+// 	users = await fetchUsers()
+// 	console.log(users)
+// 	for (
+// 		const user of users){
+// 			showList(user)
+// 		}
+// countRoles();
+// }
+// async function fetchUsers(){
+// 	const response = await fetch("users.json");
+// 	const data = await response.json();
+// 	return data
+// }
 
-function showList(user){
-	const html = /*html*/ `
-	<li>Users: ${user.name}</li>`
-	document.querySelector("#userlist").insertAdjacentHTML("beforeend",html)
-}
-function countRoles(){
-let admin = 0
-let guest = 0
-let bruger = 0 
-for (const user of users){
-if (user.role === "admin"){
-admin++
-}else if (user.role === "user"){
-bruger++
-}else if (user.role === "guest"){
-	guest++
-}}
-document.querySelector("#admin-count").textContent = admin;
-document.querySelector("#guest-count").textContent = guest;
-document.querySelector("#user-count").textContent = bruger;
-}
+// function showList(user){
+// 	const html = /*html*/ `
+// 	<li>Users: ${user.name}</li>`
+// 	document.querySelector("#userlist").insertAdjacentHTML("beforeend",html)
+// }
+// function countRoles(){
+// let admin = 0
+// let guest = 0
+// let bruger = 0
+// for (const user of users){
+// if (user.role === "admin"){
+// admin++
+// }else if (user.role === "user"){
+// bruger++
+// }else if (user.role === "guest"){
+// 	guest++
+// }}
+// document.querySelector("#admin-count").textContent = admin;
+// document.querySelector("#guest-count").textContent = guest;
+// document.querySelector("#user-count").textContent = bruger;
+// }
+
+// window.addEventListener("load", initApp);
+
+// async function initApp() {
+// 	users = await getUsers();
+// 	console.log(users);
+// 	showUsers();
+// 	createUser("sofie", true, "bitch") // opg 3 //
+// }
+// let users = [];
+
+// async function getUsers() {
+// 	const response = await fetch("users.json");
+// 	const data = response.json();
+// 	return data;
+// }
+// function showUsers() {
+// 	document.querySelector("#userlist").innerHTML = "";
+// 	for (const user of users) {
+// 		if (user.active) {
+// 			document.querySelector("#userlist").insertAdjacentElement(
+// 				"beforeend",
+// 				/*html*/ `
+// 			<li>${user.name}, ${user.active}</li>`
+// 			);
+// 		}
+// 	}
+// }
+// //Opg 3//
+// function createUser(name, active, role){
+// 	const newUser = {name: name, active: active, role: role}
+// 	users.push(newUser) // push tilføjer et element til slutningen af listen //
+// 	showUsers();
+// }
+
+
--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,2 +1,42 @@
 "use strict";
 
+window.addEventListener("load", initApp);
+
+function initApp() {
+	console.log(animals);
+	showAnimals();
+	document.querySelector("create-form").addEventListener("submit", createAnimal);
+}
+let animals = [
+	{ name: "Morten", type: "dog", age: "25" },
+	{ name: "Martin", type: "lion", age: "18" },
+	{ name: "Krystal", type: "snail", age: "30" },
+];
+
+function showAnimals() {
+	document.querySelector("tbody").innerHTML = "";
+	const sortedAnimals = animals.sort((a, b) => a.age - b.age); //sort efter alder ved at lave en arrowfunction som sammenligner alderen//
+	for (const animal of sortedAnimals) {
+		document.querySelector("tbody").insertAdjacentHTML(
+			"beforeend",
+			/*html*/ `
+	<tr>
+	<td>${animal.name}</td>
+	<td>${animal.type}</td>
+	<td>${animal.age}</td>
+	</tr>
+	`
+		);
+	}
+}
+function createAnimal(event) {
+	event.preventDefault();
+	const form = document.querySelector("create-form");
+	const name = form.name.value;
+	const type = form.type.value;
+	const age = form.age.value;
+
+	const newAnimal = { name: name, type: type, age: age };
+    animals.push(newAnimal)
+    showAnimals()
+}
