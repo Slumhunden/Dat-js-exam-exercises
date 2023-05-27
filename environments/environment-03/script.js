3f613c33e1ac33cccc041710f8217ec22a954574
Slumhunden <kroghbastin@gmail.com>
2023-05-27 08:09:51 -0400

    forsøg på øvelse 6

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+let product = [
+	{ name: "cargopants", price: 42, Boolean: true },
+	{ name: "fucks", price: 69, Boolean: false },
+	{ name: "facts", price: 100, Boolean: true },
+];
+
+function initApp() {
+	console.log("working");
+    showProduct()
+}
+function showProduct(){
+    document.querySelector("#list-container").innerHTML = "";
+    const productInStock = product.sort((a, b)=> a.true - b.false) // i tvivl om hvordan det her fungerer, den "sorter" ikke men lister i hvertfald mine items//
+    for (product of productInStock){
+        document.querySelector("#list-container").insertAdjacentHTML(
+			"beforeend",
+			/*html*/ `
+        <ul>List of Products:</ul> 
+        <li>Name of Product: ${product.name}</li>
+        <li>Price: ${product.price}</li>
+        <li>In stock? ${product.Boolean}</li>
+        
+        `
+		);
+    }  if (Boolean = true){
+        
+    }
+    // DOM mangler lidt, den viser overskriften foran hvert item//
+}
