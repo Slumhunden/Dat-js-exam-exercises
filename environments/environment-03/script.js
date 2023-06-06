2af7986e1bdf0a7480d1b08626854e298f9c2ee1
Slumhunden <kroghbastin@gmail.com>
2023-06-06 08:32:19 +0200

    usa try

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -3,9 +3,9 @@
 window.addEventListener("load", initApp);
 
 let product = [
-	{ name: "cargopants", price: 42, Boolean: true },
-	{ name: "fucks", price: 69, Boolean: false },
-	{ name: "facts", price: 100, Boolean: true },
+	{ name: "cargopants", price: 42, inStock: true },
+	{ name: "fucks", price: 69, inStock: false },
+	{ name: "facts", price: 100, inStock: true },
 ];
 
 function initApp() {
@@ -14,20 +14,19 @@ function initApp() {
 }
 function showProduct(){
     document.querySelector("#list-container").innerHTML = "";
-    const productInStock = product.sort((a, b)=> a.true - b.false) // i tvivl om hvordan det her fungerer, den "sorter" ikke men lister i hvertfald mine items//
-    for (product of productInStock){
+    for (product of product){
+        if (product.inStock === true){
         document.querySelector("#list-container").insertAdjacentHTML(
 			"beforeend",
 			/*html*/ `
         <ul>List of Products:</ul> 
         <li>Name of Product: ${product.name}</li>
         <li>Price: ${product.price}</li>
-        <li>In stock? ${product.Boolean}</li>
+        <li>In stock? ${product.inStock}</li>
         
         `
-		);
-    }  if (Boolean = true){
+		)}
+    } 
         
-    }
     // DOM mangler lidt, den viser overskriften foran hvert item//
 }
